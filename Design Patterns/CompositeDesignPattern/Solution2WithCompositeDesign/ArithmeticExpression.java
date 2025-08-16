bc779f76fda727cedd982df57311ea042d175b73
Hrishikesh Tiwari <hrishikeshtiwari98@gmail.com>
2025-08-16 17:00:04 +0530

    Delete Design Patterns/CompositeDesignPattern directory

--- Design Patterns/CompositeDesignPattern/Solution2WithCompositeDesign/ArithmeticExpression.java
+++
@@ -1,6 +0,0 @@
-package com.conceptandcoding.LowLevelDesign.DesignPatterns.CompositeDesignPattern.Solution2WithCompositeDesign;
-
-public interface ArithmeticExpression {
-
-    public int evaluate();
-}
