d851b1ca51b5857882519d65105d33afc33d2b0b
Hrishikesh Tiwari <hrishikeshtiwari98@gmail.com>
2025-08-16 17:08:31 +0530

    Delete Calculator (Composite Design Pattern) directory

--- Calculator (Composite Design Pattern)/Solution2WithCompositeDesign/Operation.java
+++
@@ -1,9 +0,0 @@
-package com.conceptandcoding.LowLevelDesign.DesignPatterns.CompositeDesignPattern.Solution2WithCompositeDesign;
-
-public enum Operation {
-
-    ADD,
-    SUBTRACT,
-    MULTIPLY,
-    DIVIDE;
-}
